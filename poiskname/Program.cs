e8beb246e69e81942d5c5520477bb6b72dbf63d3
Лера Голикова <funtik-98@mail.ru>
2023-05-24 12:01:43 +0700

    Добавьте файлы проекта.

---
+++ poiskname/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Создание списка имен
+        List<string> names = new List<string>()
+        {
+            "Коля",
+            "Петя",
+            "Вера",
+            "Надежда",
+            "Любовь",
+            "Сергей"
+        };
+
+        // Сортировка списка имен по алфавиту
+        names.Sort();
+
+        // Запрос имени для поиска
+        Console.WriteLine("Введите имя для поиска:");
+        string searchName = Console.ReadLine();
+
+        // Использование метода BinarySearch для поиска имени в списке
+        int index = names.BinarySearch(searchName);
+
+        // Проверка результатов поиска
+        if (index >= 0)
+        {
+            Console.WriteLine("Имя найдено!");
+            Console.WriteLine(names[index]);
+        }
+        else
+        {
+            Console.WriteLine("Имя не найдено!");
+            Console.WriteLine("0");
+        }
+    }
+}
